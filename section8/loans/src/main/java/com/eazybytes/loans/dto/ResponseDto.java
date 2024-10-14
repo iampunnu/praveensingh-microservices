041b8c6ad7d6f4216712bc2fa47535932cbe6b68
praveen singh <praveensingh@praveens-MacBook-Air.local>
2024-10-14 08:24:39 +0530

    completechange

---
+++ section8/loans/src/main/java/com/eazybytes/loans/dto/ResponseDto.java
@@ -0,0 +1,13 @@
+package com.eazybytes.loans.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class ResponseDto {
+
+    public String statusCode;
+
+    public String statusMsg;
+
+}
